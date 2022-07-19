7db83385bfc1b0b1ce726c8afd54dad857379598
James Abel <j@abel.co>
2022-07-19 15:19:18 -0700

    initial commit

--- scripts/make_venv_dev.bat
+++ scripts/make_venv_dev.bat
@@ -7,5 +7,6 @@ call venv\Scripts\activate.bat
 python -m pip install --no-deps --upgrade pip
 python -m pip install -U setuptools
 python -m pip install -U -r requirements-dev.txt
+pre-commit install
 popd
 call deactivate

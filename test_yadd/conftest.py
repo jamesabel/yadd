151ff85743ea606a9094af93a8b35602c09393ad
James Abel <j@abel.co>
2022-09-12 16:29:54 -0700

    get version dynamically in pyproject.toml for both setuptools and hatch

--- test_yadd/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from yadd import make_versions_file
-
-
-@pytest.fixture(scope="session", autouse=True)
-def session_fixture():
-    make_versions_file()  # not really a test, but automatically keep version file up to date
--- yadd/__init__.py
+++ yadd/__init__.py
@@ -1,4 +1,3 @@
-from .__version__ import application_name, author, version
-from .make_versions_file import make_versions_file
+from .__version__ import application_name, author, version, __version__
 from .yadd_module import yadd, Yadd, MiscompareException
 from .yadd_cli import yadd_cli
